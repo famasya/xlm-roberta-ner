b2e4348f3c05a95da77588ab2cc23f3ec2325811
Abid Famasya <abid.famasya@gmail.com>
2021-08-18 21:40:57 +0700

    change entities type

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -55,7 +55,7 @@ class NerProcessor:
             self._read_file(os.path.join(data_dir, "test.txt")), "test")
 
     def get_labels(self):
-        return ["O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-MISC", "I-MISC"]
+        return ["O", "B-DISO", "I-DISO", "B-CHEM", "I-CHEM", "B-ANAT", "I-ANAT", "B-PROC", "I-PROC"]
 
     def _read_file(self, filename):
         '''
